679b4fbeed82178dbb238aac5cee2e96ff67d912
Mayokun Adeniyi <adeniyimayokun17@gmail.com>
2019-10-01 00:16:39 +0100

    feat: add Navigation Component to Android Trivia

--- andfun-kotlin-android-trivia-starter-code/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ andfun-kotlin-android-trivia-starter-code/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -8,6 +8,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
@@ -20,6 +23,8 @@ class TitleFragment : Fragment() {
         val binding : FragmentTitleBinding = DataBindingUtil.inflate(
                 inflater,R.layout.fragment_title,container,false
         )
+        binding.playButton.setOnClickListener(
+                Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
         return binding.root
     }
 
