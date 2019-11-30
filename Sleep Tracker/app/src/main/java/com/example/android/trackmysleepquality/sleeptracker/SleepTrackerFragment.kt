6d58bec261e7f4322760e4daad777ca43042fa48
mayokunthefirst <adeniyimayokun17@gmail.com>
2019-11-30 21:01:36 +0100

    Feat: change layout manager to grid layout manager

--- Sleep Tracker/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ Sleep Tracker/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -25,6 +25,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.database.SleepDatabase
 import com.example.android.trackmysleepquality.databinding.FragmentSleepTrackerBinding
@@ -62,6 +63,9 @@ class SleepTrackerFragment : Fragment() {
         binding.sleepTrackerViewModel = sleepTrackerViewModel
         val adapter = SleepNightAdapter()
         binding.sleepRecyclerView.adapter = adapter
+
+        val layoutManager = GridLayoutManager(context,3)
+        binding.sleepRecyclerView.layoutManager = layoutManager
 
         sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
             it?.let {
