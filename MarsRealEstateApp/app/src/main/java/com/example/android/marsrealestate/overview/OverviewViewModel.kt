f65a57119a80e03f45fe39443d7cee33bc11b29e
mayokunthefirst <adeniyimayokun17@gmail.com>
2019-12-03 05:05:31 +0100

    Remove callbacks and add coroutine support

--- MarsRealEstateApp/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ MarsRealEstateApp/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -21,10 +21,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.android.marsrealestate.network.MarsApi
-import com.example.android.marsrealestate.network.MarsProperty
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
@@ -38,6 +38,10 @@ class OverviewViewModel : ViewModel() {
     val response: LiveData<String>
         get() = _response
 
+    //Coroutine Job and Coroutine Scope
+    val coroutineJob = Job()
+    val coroutineScope = CoroutineScope(Dispatchers.Main + coroutineJob)
+
     /**
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
      */
@@ -49,15 +53,18 @@ class OverviewViewModel : ViewModel() {
      * Sets the value of the status LiveData to the Mars API status.
      */
     private fun getMarsRealEstateProperties() {
-        MarsApi.retrofitService.getProperties().enqueue(object: Callback<List<MarsProperty>>{
-            override fun onFailure(call: Call<List<MarsProperty>>, t: Throwable) {
-                _response.value = "Failure" + t.message
-            }
+      coroutineScope.launch {
+          val getPropertiesDeferred = MarsApi.retrofitService.getProperties()
+          try {
+              _response.value = "Success ${getPropertiesDeferred.size} Mars properties retrieved"
+          }catch (t: Throwable){
+              _response.value = "Failure" + t.message
+          }
+      }
+    }
 
-            override fun onResponse(call: Call<List<MarsProperty>>, response: Response<List<MarsProperty>>) {
-                _response.value = "Success ${response.body()?.size} Mars properties retrieved"
-            }
-        })
-        _response.value = "Set the Mars API Response here!"
+    override fun onCleared() {
+        super.onCleared()
+        coroutineJob.cancel()
     }
 }
