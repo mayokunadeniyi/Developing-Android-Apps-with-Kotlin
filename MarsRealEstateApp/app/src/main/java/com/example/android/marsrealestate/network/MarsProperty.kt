53421f0713a061a3623de02cd53f9af628e5314c
mayokunthefirst <adeniyimayokun17@gmail.com>
2019-12-07 21:08:21 +0100

    Make mars property parcelable

--- MarsRealEstateApp/app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
+++ MarsRealEstateApp/app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
@@ -17,11 +17,14 @@
 
 package com.example.android.marsrealestate.network
 
+import android.os.Parcelable
 import com.squareup.moshi.Json
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class MarsProperty(
         val id: String,
         @Json(name = "img_src") val imgSrcUrl: String,
         val type: String,
         val price: Double
-)
+) : Parcelable
