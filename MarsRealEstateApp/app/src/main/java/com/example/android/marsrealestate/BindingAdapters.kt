4dd81b34fdd64ef689c168404bff84a7da9f3042
mayokunthefirst <adeniyimayokun17@gmail.com>
2019-12-07 21:01:39 +0100

    Add bind status to binding adapter

--- MarsRealEstateApp/app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
+++ MarsRealEstateApp/app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
@@ -17,12 +17,22 @@
 
 package com.example.android.marsrealestate
 
+import android.view.View
 import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.example.android.marsrealestate.network.MarsProperty
+import com.example.android.marsrealestate.overview.MarsApiStatus
+import com.example.android.marsrealestate.overview.PhotoGridAdapter
 
+@BindingAdapter("listData")
+fun bindRecyclerView(recyclerView: RecyclerView, data: List<MarsProperty>?){
+    val adapter = recyclerView.adapter as PhotoGridAdapter
+    adapter.submitList(data)
+}
 
 @BindingAdapter("imageUrl")
 fun ImageView.bindImage(imgUrl: String?){
@@ -36,4 +46,23 @@ fun ImageView.bindImage(imgUrl: String?){
                 .into(this)
     }
 }
+
+@BindingAdapter("marsApiStatus")
+fun ImageView.bindStatus(status: MarsApiStatus?){
+    when(status){
+        MarsApiStatus.LOADING -> {
+            this.visibility = View.VISIBLE
+            this.setImageResource(R.drawable.loading_animation)
+        }
+        MarsApiStatus.ERROR -> {
+            this.visibility = View.VISIBLE
+            this.setImageResource(R.drawable.ic_connection_error)
+        }
+        MarsApiStatus.DONE -> {
+            this.visibility = View.GONE
+        }
+    }
+}
+
+
 
