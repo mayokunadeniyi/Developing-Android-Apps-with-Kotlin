d8651936c38e635a9420b8deeee433053b177242
mayokunthefirst <adeniyimayokun17@gmail.com>
2019-12-07 21:03:47 +0100

    Add photogrid adapter

--- MarsRealEstateApp/app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
+++ MarsRealEstateApp/app/src/main/java/com/example/android/marsrealestate/overview/OverviewFragment.kt
@@ -43,13 +43,15 @@ class OverviewFragment : Fragment() {
      */
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val binding = GridViewItemBinding.inflate(inflater)
+        val binding = FragmentOverviewBinding.inflate(inflater)
 
         // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
         binding.setLifecycleOwner(this)
 
         // Giving the binding access to the OverviewViewModel
         binding.viewModel = viewModel
+
+        binding.photosGrid.adapter = PhotoGridAdapter()
 
         setHasOptionsMenu(true)
         return binding.root
--- MarsRealEstateApp/app/src/main/java/com/example/android/marsrealestate/overview/PhotoGridAdapter.kt
+++ MarsRealEstateApp/app/src/main/java/com/example/android/marsrealestate/overview/PhotoGridAdapter.kt
@@ -16,3 +16,43 @@
  */
 
 package com.example.android.marsrealestate.overview
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.marsrealestate.databinding.GridViewItemBinding
+import com.example.android.marsrealestate.network.MarsProperty
+
+class PhotoGridAdapter: ListAdapter<MarsProperty,PhotoGridAdapter.MarsPropertyViewHolder>(DiffCallBack){
+
+    class MarsPropertyViewHolder(private val binding: GridViewItemBinding)
+        : RecyclerView.ViewHolder(binding.root) {
+        fun bind(marsProperty: MarsProperty){
+            binding.property = marsProperty
+            binding.executePendingBindings()
+        }
+    }
+
+    companion object DiffCallBack: DiffUtil.ItemCallback<MarsProperty>() {
+        override fun areItemsTheSame(oldItem: MarsProperty, newItem: MarsProperty): Boolean {
+            return oldItem === newItem
+        }
+
+        override fun areContentsTheSame(oldItem: MarsProperty, newItem: MarsProperty): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MarsPropertyViewHolder {
+        return MarsPropertyViewHolder(GridViewItemBinding.inflate(LayoutInflater.from(parent.context)))
+    }
+
+    override fun onBindViewHolder(holder: MarsPropertyViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item)
+    }
+
+}
