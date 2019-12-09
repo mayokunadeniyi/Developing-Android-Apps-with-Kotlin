fffd5d6f431d370c35d087e2390f5e239ede2aef
mayokunthefirst <adeniyimayokun17@gmail.com>
2019-12-09 05:13:26 +0100

    Add transformation to marsproperty detail

--- MarsRealEstateApp/app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
+++ MarsRealEstateApp/app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
@@ -27,4 +27,7 @@ data class MarsProperty(
         @Json(name = "img_src") val imgSrcUrl: String,
         val type: String,
         val price: Double
-) : Parcelable
+) : Parcelable{
+    val isRental
+    get() = type == "rent"
+}
